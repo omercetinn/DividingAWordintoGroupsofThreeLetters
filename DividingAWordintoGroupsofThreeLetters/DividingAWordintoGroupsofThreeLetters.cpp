f89d20e0bfba345e14b195df8f65cc256f76eb82
ömer <mer@DESKTOP-6S1VIE5>
2022-10-22 12:14:16 +0300

    fix

--- DividingAWordintoGroupsofThreeLetters/DividingAWordintoGroupsofThreeLetters.cpp
+++ DividingAWordintoGroupsofThreeLetters/DividingAWordintoGroupsofThreeLetters.cpp
@@ -5,16 +5,26 @@ using namespace std;
 
 int main()
 {
-	string temp = "";
-	string str("0123456789asdf@");
-	for (int i = 0; i < str.size(); ++i) {
-		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') ||
-			(str[i] >= '0' && str[i] <= '9')) {
-			temp = temp + str[i];
-		}
-	}
-	str = temp;
-	for (int i = 0; i < temp.length(); i += 3) {
-		cout << temp.substr(i, 3) << endl;
-	}
+    string temp = "";
+    string S("23456789ASD3@"); //S values manuel input
+    //string S; // S values external input
+    //cout<<"Enter your S value: "; 
+    //cin>>S;
+    if (S.size() > 2)
+    {
+        for (int i = 0; i < S.size(); ++i) { // examining letter one by one and removing unwanted letters
+            if ((S[i] >= 'a' && S[i] <= 'z') || (S[i] >= 'A' && S[i] <= 'Z') ||
+                (S[i] >= '0' && S[i] <= '9')) {
+                temp = temp + S[i];
+            }
+        }
+        S = temp;
+        for (int i = 0; i < temp.length(); i += 3) {
+            cout << temp.substr(i, 3) << ' '; //taking printouts in triplicate
+        }
+
+    }
+    else {
+        cout << "Please min 2 character input! ";
+    }
 }
